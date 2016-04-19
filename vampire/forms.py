7605155278005961087262716cc72ca727a8bd4e
sidn77 <siddhantn77@gmail.com>
2016-04-19 17:25:46 +0530

    added database dump

--- vampire/forms.py
+++ vampire/forms.py
@@ -1,3 +1,16 @@
 from django import forms
 from .forms import *
+from .models import *
+
+class DonorForm(forms.ModelForm):
+    class Meta:
+        model = Donor
+        fields = '__all__'
+        exclude = ['aid']
+
+class AddressForm(forms.ModelForm):
+        class Meta:
+            model = Address
+            fields = '__all__'
+            exclude = ['aid']
 
